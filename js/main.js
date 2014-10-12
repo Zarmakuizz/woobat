4e09977505bb73889a9d51eaeac5df8d4f48e338
Zarmakuizz <generalzarmakuizz@hotmail.fr>
2014-10-12 14:59:02 +0200

    Starting the [known players] table
    Done:
    - it's here.
    - adding a player will add a row to this table
    To do:
    - players.xml management (load/save)
    - checks (if player is already in the file, checks on player's inputs)
    - Player declaration should be filled when pairing algorithm will be there.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 // Class, methods, etc
+/** Class representing a player */
+function Player(pid, firstName, lastName, birthDate) {
+    this.pid = pid;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthDate = birthDate; // TODO manage date
+    this.ageGroup = getAgeDivision(birthDate);
+    // TODO add pairing management
+}
 
 
+/** Event fired when a player is added to the tournament
+    using the add form. */
 var addFormPlayer = function(evt){
     // Check if the form has some empty values or incorrect ones
     var addForm = document.querySelector('#add_player_form');
@@ -15,29 +26,41 @@ var addFormPlayer = function(evt){
     // TODO check pid and birth_date values
 
     // Here it seems that all field have been provided with value
+    var newPlayer = new Player(
+        addForm.querySelector('#pid').value,
+        addForm.querySelector('#first_name').value,
+        addForm.querySelector('#last_name').value,
+        addForm.querySelector('#birth_date').value);
+    var known = document.querySelector('#known tbody');
     var registered = document.querySelector('#registered tbody');
     var newRow = registered.insertRow(registered.rows.length);
     
-    var newCell = newRow.insertCell(0);
-    var newText = document.createTextNode(addForm.querySelector('#pid').value);
+    var newCell = newRow.insertCell(0); // TODO better insertion
+    var newText = document.createTextNode(newPlayer.pid);
     newCell.appendChild(newText);
     
     newCell = newRow.insertCell(1);
-    newText = document.createTextNode(addForm.querySelector('#first_name').value);
+    newText = document.createTextNode(newPlayer.firstName);
     newCell.appendChild(newText);
     
     newCell = newRow.insertCell(2);
-    newText = document.createTextNode(addForm.querySelector('#last_name').value);
+    newText = document.createTextNode(newPlayer.lastName);
     newCell.appendChild(newText);
     
     newCell = newRow.insertCell(3);
-    newText = document.createTextNode(addForm.querySelector('#birth_date').value);
+    newText = document.createTextNode(newPlayer.birthDate);
     newCell.appendChild(newText);
     
     newCell = newRow.insertCell(4);
-    newText = document.createTextNode(getAgeDivision(addForm.querySelector('#first_name').value));
+    newText = document.createTextNode(newPlayer.ageGroup);
     newCell.appendChild(newText);
     
+    // TODO check if the player isn't already known
+    // TODO add player to xml
+    var copyRow = newRow.cloneNode(true);
+    known.appendChild(copyRow);
+    
+    // Clean out the add form
     setTimeout(function(){addForm.reset();},50);
 }
 var playerRegSelect = function(elt){
