eb28ec37a1c77f6833595fa116a0ffa88d44a87b
Zarmakuizz <generalzarmakuizz@hotmail.fr>
2014-10-29 22:24:51 +0100

    Last form: add basic display of age groups
    TODO: a JS object to manage age groups methods, such as which age groups to play, how many rounds, etc.

--- js/main.js
+++ js/main.js
@@ -53,7 +53,6 @@ var addPlayer = function(player, id, event){
     }
 };
 var addRegPlayer = function(player){
-    console.log(player+"; "+player.ageGroup.toLowerCase());
     var totalPlayers = document.querySelector('#reg_total').innerHTML;
     document.querySelector('#reg_total').innerHTML = parseInt(totalPlayers)+1;
     if(parseInt(totalPlayers)+1 >= 8){
@@ -68,6 +67,131 @@ var addRegPlayer = function(player){
 var addKnownPlayer = function(player){
     addPlayer(player,'#known',playerKnownSelect);
 };
+
+/** Class representing an element of the list of possible age divisions.
+    AgeDivision's constructor also initializes the relevant number of rounds.
+ */
+function AgeDivision(nbPlayers, roundMode, hasTops){
+    this.nbJuniors = function(){throw Error('This should have been overriden if relevant.')};
+    this.nbSeniors = function(){throw Error('This should have been overriden if relevant.')};
+    this.nbMasters = function(){throw Error('This should have been overriden if relevant.')};
+    this.nbPlayers = nbPlayers;
+    this.hasTops = hasTops;
+    AgeDivision(nbPlayers, roundMode, hasTops); // FIXME inheritance in Javascript
+    if(roundMode == "single"){
+        var i=0;
+        do{i++}while(nbPlayers/Math.pow(2,i) > 1.0);
+        this.nbRoundsTop=0;
+        this.nbRounds = i;
+        this.hasDay2 = false;
+    }else if(roundMode == "swiss"){
+        // TODO read rules
+        if(nbPlayers <= 8){
+            nbRounds = 3;
+            nbRoundsTop = 0;
+            this.hasDay2 = false;
+            this.day2Rounds = 0;
+        }else if(nbPlayers <= 12){
+            nbRounds = 4;
+            nbRoundsTop = (hasTops ? 2 : 0);
+            this.hasDay2 = false;
+            this.day2Rounds = 0;
+        }else if(nbPlayers <= 20){
+            nbRounds = 5;
+            nbRoundsTop = (hasTops ? 2 : 0);
+            this.hasDay2 = false;
+            this.day2Rounds = 0;
+        }else if(nbPlayers <= 32){
+            nbRounds = 5;
+            nbRoundsTop = (hasTops ? 3 : 0);
+            this.hasDay2 = false;
+            this.day2Rounds = 0;
+        }else if(nbPlayers <= 64){
+            nbRounds = 6;
+            nbRoundsTop = (hasTops ? 3 : 0);
+            this.hasDay2 = false;
+            this.day2Rounds = 0;
+        }else if(nbPlayers <= 128){
+            nbRounds = 7;
+            nbRoundsTop = (hasTops ? 3 : 0);
+            this.hasDay2 = false;
+            this.day2Rounds = 0;
+        }else if(nbPlayers <= 227){// Not sure about that number
+            nbRounds = 8;
+            nbRoundsTop = (hasTops ? 3 : 0);
+            this.hasDay2 = false;
+            this.day2Rounds = 0;
+        }else if(true /* TODO */){
+            // TODO
+            nbRounds = 9;
+            nbRoundsTop = (hasTops ? 3 : throw Error('This case might not be covered by the rules.'));
+            this.hasDay2 = true;
+            this.day2Rounds = 5;
+        }
+    }else{
+        throw Error('We don\'t know which round mode you are talking about');
+    }
+};
+function AgeJunior(nbPlayers, roundMode, hasTops){
+    AgeDivision(nbPlayers, roundMode, hasTops);// TODO check inheritance system
+    this.nbJuniors = nbPlayers;
+}
+function AgeSenior(nbPlayers, roundMode, hasTops){
+    AgeDivision(nbPlayers, roundMode, hasTops);// TODO check inheritance system
+    this.nbSeniors = nbPlayers;
+}
+function AgeMaster(nbPlayers, roundMode, hasTops){
+    AgeDivision(nbPlayers, roundMode, hasTops);// TODO check inheritance system
+    this.nbMasters = nbMasters;
+}
+function AgeJuniorSenior(nbJuniors, nbSeniors, roundMode, hasTops){
+    // Only one of the two age divisions can be big enough to still have tops in swiss, despite being mixed to the other age division.
+    if(nbJuniors > 8 && roundMode == "swiss"){
+        AgeDivision(nbJuniors, roundMode, hasTops);
+    }else if(nbSeniors > 8 && roundMode == "swiss"){
+        AgeDivision(nbSeniors, roundMode, hasTops);
+    }else if(roundMode == "single"){
+        AgeDivision(nbJuniors+nbSeniors, roundMode, hasTops);
+    }else{
+        AgeDivision(nbJuniors, roundMode, false);
+    }
+    this.nbJuniors = nbJuniors;
+    this.nbSeniors = nbSeniors;
+    this.nbPlayers = nbJuniors + nbSeniors;
+}
+function AgeSeniorMaster(nbSeniors, nbMasters, roundMode, hasTops){
+    // Only one of the two age divisions can be big enough to still have tops in swiss, despite being mixed to the other age division.
+    if(nbSeniors > 8 && roundMode == "swiss"){
+        AgeDivision(nbSeniors, roundMode, hasTops);
+    }else if(nbMasters > 8 && roundMode == "swiss"){
+        AgeDivision(nbMasters, roundMode, hasTops);
+    }else if(roundMode == "single"){
+        AgeDivision(nbSeniors+nbMasters, roundMode, hasTops);
+    }else{
+        AgeDivision(nbSeniors, roundMode, false);
+    }
+    this.nbSeniors = nbSeniors;
+    this.nbMasters = nbMasters;
+    this.nbPlayers = nbSeniors + nbMasters;
+}
+function AgeAll(nbJuniors, nbSeniors, nbMasters, roundMode, hasTops){
+    // Only one of the two age divisions can be big enough to still have tops in swiss, despite being mixed to the other age division.
+    if(nbJuniors > 8 && roundMode == "swiss"){
+        AgeDivision(nbJuniors, roundMode, hasTops);
+    }else if(nbSeniors > 8 && roundMode == "swiss"){
+        AgeDivision(nbSeniors, roundMode, hasTops);
+    }else if(nbMasters > 8 && roundMode == "swiss"){
+        AgeDivision(nbMasters, roundMode, hasTops);
+    }else if(roundMode == "single"){
+        AgeDivision(nbJuniors+nbSeniors+nbMasters, roundMode, hasTops);
+    }else{
+        AgeDivision(nbJuniors, roundMode, false);
+    }
+    this.nbJuniors = nbJuniors;
+    this.nbSeniors = nbSeniors;
+    this.nbMasters = nbMasters;
+    this.nbPlayers = nbJuniors + nbSeniors + nbMasters;
+}
 /** Checks the date to determine the age division.
     Age division is based solely on birthdate's year.
     However, around July 15th (enough days after US nationals), every 
@@ -90,7 +214,44 @@ var getAgeDivision = function(date){
     if(age >=11 && age <= 14){ return 'Senior';}
     return 'Master';
 };
+/** Determines the allowed categories and the number of rounds that should be played.
+    TODO unit testing : no more than 3 age groups, no collision, right group construction.
+    TODO defensive programming, we are assuming at least 8 players.
+    @param nbJuniors number of registered juniors
+    @param nbSeniors number of registered seniors
+    @param nbMasters number of registered masters
+    @return array of bools deciding if a row should be displayed.
+    */
+var getDivisions = function(nbJuniors, nbSeniors, nbMasters){
+    var result = [false,false,false,false,false,false];
+    // In which group do the Juniors go?
+    if(nbJuniors >=6 && nbSeniors + nbMasters >= 6){ 
+        // Juniors may go alone
+        result[0]=true;
+    }else if(nbJuniors <6 && nbJuniors + nbSeniors >= 6 && nbMasters >= 6){
+        // Juniors aren't enough alone, they are mixed with Seniors
+        // (implies Seniors not alone and Masters alone)
+        result[2]=true;
+    }else{
+        // Juniors might be big enough but there is a lack of older players
+        // or not enough players (implies no age group alone)
+        result[5]=true;
+    }
+    // In which group do the Seniors go? (partially already answered)
+    if(nbJuniors >=6 && nbSeniors >= 6 && nbMasters >=6){
+        // Seniors may go alone (implies Juniors and Masters are alone too)
+        result[1]=true
 
+    }else if(nbJuniors >= 6 && nbSeniors + nbMasters >=6){
+        // Seniors are mixed to masters (implies Juniors alone)
+        result[4]=true;
+    }
+    if(nbJuniors + nbSeniors >=6 && nbMasters >=6){
+        // Masters may go alone
+        result[3]=true;
+    }
+    return result;
+};
 // Transition methods 
 /** Select a file when clicking on "load players.xml" */
 var loadPlayerFile = function(evt){
@@ -202,7 +363,73 @@ var playerRegSelect = function(elt){
         elt.className+=' selected';
     }
 };
-
+/** When Swiss is chosen */
+var onSwissEvent = function(evt){
+    console.log("Swiss mode yeah");
+    // TODO manage consequences on round calculation for players
+    var form = document.querySelector('#pairing_system');
+    var radio = form.querySelectorAll('input[name="pairing_top"]');
+    radio[0].disabled = false;
+    radio[1].disabled = false;
+    if(radio[0].checked){ // If there are tops, select next options
+        radio = form.querySelectorAll('input[name="small_final"]');
+        radio[0].disabled = false;
+        radio[1].disabled = false;
+        /*
+        radio = form.querySelectorAll('input[name="day2"]');
+        radio[0].disabled = false;
+        radio[1].disabled = false;
+        //*/
+    }else{
+        radio = form.querySelectorAll('input[name="small_final"]');
+        radio[0].disabled = true;
+        radio[1].disabled = true;
+    }
+};
+var onSingleEvent = function(evt){
+    console.log("Single Elimination mode yeah");
+    // TODO manage consequences on round calculation for players
+    var form = document.querySelector('#pairing_system');
+    var radio = form.querySelectorAll('input[name="pairing_top"]');
+    radio[0].disabled = true;
+    radio[1].disabled = true;
+    radio = form.querySelectorAll('input[name="small_final"]');
+    radio[0].disabled = false;
+    radio[1].disabled = false;
+    /*
+    radio = form.querySelectorAll('input[name="day2"]');
+    radio[0].disabled = true;
+    radio[1].disabled = true;
+    //*/
+};
+var onSwissTop = function(evt){
+    var form = document.querySelector('#pairing_system');
+    radio = form.querySelectorAll('input[name="small_final"]');
+    radio[0].disabled = false;
+    radio[1].disabled = false;
+    /*
+    radio = form.querySelectorAll('input[name="day2"]');
+    radio[0].disabled = false;
+    radio[1].disabled = false;
+    //*/
+    radio = form.querySelectorAll('input[name="less_round"]');
+    radio[0].disabled = false;
+    radio[1].disabled = false;
+}
+var onSwissNoTop = function(evt){
+    var form = document.querySelector('#pairing_system');
+    radio = form.querySelectorAll('input[name="small_final"]');
+    radio[0].disabled = true;
+    radio[1].disabled = true;
+    /*
+    radio = form.querySelectorAll('input[name="day2"]');
+    radio[0].disabled = true;
+    radio[1].disabled = true;
+    //*/
+    radio = form.querySelectorAll('input[name="less_round"]');
+    radio[0].disabled = true;
+    radio[1].disabled = true;
+}
 /** Event fired when the first view is done. */
 var idToPlayers = function(evt){
     var initForm = document.querySelector('#id_form');
@@ -220,10 +447,69 @@ var idToPlayers = function(evt){
     document.querySelector('#add_players').style.display="block";
     document.querySelector('#home').style.display="none";
 };
+/** Switches view.
+ */
 var playersToRecap = function(evt){
+    // Count players and display relevant categories
+    var nbJuniors = parseInt(document.querySelector('#reg_junior').innerHTML);
+    var nbSeniors = parseInt(document.querySelector('#reg_senior').innerHTML);
+    var nbMasters = parseInt(document.querySelector('#reg_master').innerHTML);
+    var divisions = document.querySelectorAll('#divisions_recap li');
+    var show = getDivisions(nbJuniors, nbSeniors, nbMasters);
+    if(show[0]){
+        divisions[0].style.display="block";
+        divisions[0].querySelectorAll('span')[0].innerHTML = nbJuniors;
+        divisions[0].querySelectorAll('input[type="number"]')[0].min="3";
+        divisions[0].querySelectorAll('input[type="number"]')[0].max="3";
+        //divisions[0].querySelectorAll('input[type="number"]')[0].value="3";
+        // TODO calculate min and max for input
+        // FIXME: TOM has strange behaviors…
+    }else{
+        divisions[0].style.display="none";
+        divisions[0].querySelectorAll('span')[0].innerHTML = 0;
+        divisions[0].querySelectorAll('input[type="number"]')[0].min="0";
+        divisions[0].querySelectorAll('input[type="number"]')[0].max="0";
+        divisions[0].querySelectorAll('input[type="number"]')[0].value="0";
+    }
+    if(show[1]){
+        divisions[1].style.display="block";
+        divisions[1].querySelectorAll('span')[0].innerHTML = nbSeniors;
+    }else{
+        divisions[1].style.display="none";
+        divisions[1].querySelectorAll('span')[0].innerHTML = 0;
+    }
+    if(show[2]){
+        divisions[2].style.display="block";
+        divisions[2].querySelectorAll('span')[0].innerHTML = nbJuniors + nbSeniors;
+    }else{
+        divisions[2].style.display="none";
+        divisions[2].querySelectorAll('span')[0].innerHTML = 0;
+    }
+    if(show[3]){
+        divisions[3].style.display="block";
+        divisions[3].querySelectorAll('span')[0].innerHTML = nbMasters;
+    }else{
+        divisions[3].style.display="none";
+        divisions[3].querySelectorAll('span')[0].innerHTML = 0;
+    }
+    if(show[4]){
+        divisions[4].style.display="block";
+        divisions[4].querySelectorAll('span')[0].innerHTML = nbSeniors + nbMasters;
+    }else{
+        divisions[4].style.display="none";
+        divisions[4].querySelectorAll('span')[0].innerHTML = 0;
+    }
+    if(show[5]){
+        divisions[5].style.display="block";
+        divisions[5].querySelectorAll('span')[0].innerHTML = nbJuniors + nbSeniors + nbMasters;
+    }else{
+        divisions[5].style.display="none";
+        divisions[5].querySelectorAll('span')[0].innerHTML = 0;
+    }
+    // Update view
     document.querySelector('#recap').style.display="block";
     document.querySelector('#add_players').style.display="none";
-}
+};
 // Event handlers
 document.querySelector('#id_form').action="javascript:void(0);";
 document.querySelector('#id_form_submit').addEventListener('click', idToPlayers);
@@ -239,3 +525,13 @@ document.querySelector('#export_players_xml').addEventListener('touchend',export
 document.querySelector('#add_player_form').action="javascript:void(0);";
 document.querySelector('#add_form_submit').addEventListener('click', addFormPlayer);
 document.querySelector('#add_form_submit').addEventListener('touchend', addFormPlayer);
+
+document.querySelector('#radio_swiss').addEventListener('click',onSwissEvent);
+document.querySelector('#radio_swiss').addEventListener('touchend',onSwissEvent);
+document.querySelector('#radio_single').addEventListener('click',onSingleEvent);
+document.querySelector('#radio_single').addEventListener('touchend',onSingleEvent);
+document.querySelector('#pairing_top_yes').addEventListener('click',onSwissTop);
+document.querySelector('#pairing_top_yes').addEventListener('touchend',onSwissTop);
+document.querySelector('#pairing_top_no').addEventListener('click',onSwissNoTop);
+document.querySelector('#pairing_top_no').addEventListener('touchend',onSwissNoTop);
+
